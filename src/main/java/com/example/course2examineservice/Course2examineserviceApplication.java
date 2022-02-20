f9e5136107a42f571eb4de653575dbe6bdf76977
AnnaSergeenkova <lutekAnn@yandex.ru>
2022-02-20 10:44:12 +0300

    start

---
+++ src/main/java/com/example/course2examineservice/Course2examineserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.course2examineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2examineserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2examineserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/course2examineservice/Course2examineserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course2examineservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2examineserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
